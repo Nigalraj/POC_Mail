d2fa57fa78a462846f26f630b89cc8f8beb12041
Jegan <jeganeeemkce@gmail.com>
2023-10-25 16:01:35 +0530

    change

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,3 @@
-export const Pain = "../../src/assets/Pain.jpg";
-export const Minato = "./../src/assets/MINATOFEAT.jpg";
-export const Madara = "./../src/assets/madara.jpg";
 
-export const image = [Pain, Minato, Madara];
+
+export const image = ["../../src/assets/Pain.jpg", "./../src/assets/MINATOFEAT.jpg", "./../src/assets/madara.jpg"];
