e00c26c51a9607b0b8bf9b8fc6cef2ecbe489b1d
Jegan <jeganeeemkce@gmail.com>
2023-10-27 19:48:54 +0530

    changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -23,7 +23,7 @@ function Header() {
                                     onClick={() => {
                                         setActive(i);
                                         if (active === 0) {
-                                            nav("/dashboard");
+                                            nav("/");
                                         }
                                     }}
                                 >
