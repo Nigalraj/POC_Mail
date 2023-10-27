be222f441a57af06d825bc93390da7c2bc6ed491
Jegan <jeganeeemkce@gmail.com>
2023-10-27 20:13:39 +0530

    path change

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export const App = () => {
                 {show && <Header />}
                 <Routes>
                     <>
-                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
                         <Route path="/signup" element={<Signup />} />
                         <Route path="/dashboard" element={<Dashboard />} />
                         <Route path="/edit" element={<Template />} />
